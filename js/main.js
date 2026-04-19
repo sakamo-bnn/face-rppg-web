5556348caebc5dad2aa7e197282dbe8c8e8b3787
sakamo-bnn <90294278+sakamo-bnn@users.noreply.github.com>
2026-04-19 22:02:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,8 +42,8 @@ async function startCamera() {
         stream = await navigator.mediaDevices.getUserMedia({
             video: {
                 facingMode: "user",
-                width: { ideal: 640 },
-                height: { ideal: 480 }
+                // width: { ideal: 640 },
+                // height: { ideal: 480 }
             },
             audio: false
         });
